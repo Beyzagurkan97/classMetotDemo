4f41c6e12ad28b3bda7b3d6147edaed4bc0f131a
Beyzagurkan97 <beyzagurkan97@gmail.com>
2021-01-27 19:21:49 +0300

    ilk demo

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void Ekleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " yeni müşteri eklendi.Yeni müşterinin mesleği: " + musteri.Meslek);
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " müşteri silindi");
+        }
+        public void Listeleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşterinin  Adı:" + musteri.Ad);
+            Console.WriteLine("Müşterinin  Soyadı:" + musteri.Soyad);
+            Console.WriteLine("Müşterinin  ID:" + musteri.Id);
+            Console.WriteLine("Müşterinin  Mesleği:" + musteri.Meslek);
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Beyza";
+            musteri1.Soyad = "Can";
+            musteri1.Meslek = "Yazılım Mühendisliği";
+            musteri1.Id = 1;
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Osman";
+            musteri2.Soyad = "Bawa";
+            musteri2.Meslek = "Muhasebeci";
+            musteri2.Id = 2;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Ayşe";
+            musteri3.Soyad = "Özdil";
+            musteri3.Meslek = "Öğretmen";
+            musteri3.Id = 3;
+
+            MusteriManager musteriyoneticisi = new MusteriManager();
+            musteriyoneticisi.Ekleme(musteri1);
+            Console.WriteLine("-----");
+            musteriyoneticisi.Listeleme(musteri2);
+            Console.WriteLine("-----");
+            musteriyoneticisi.Silme(musteri3);
+
+        }
+    }
+}
